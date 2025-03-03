1914116ded287b0269d19749fb85df1243589a5c
wakoIniad <s1f102400606@iniad.org>
2025-03-04 04:07:23 +0900

    lowだけが通るサンプル

--- robot/sketch.js
+++ robot/sketch.js
@@ -7,9 +7,9 @@ let resolution = 2; // セルの大きさ
 
 //discreat param
 var dx = 0.1;
-var dt = 0.001  //0001;
+var dt = 0.0001;
 let time = 0;
-let lambda = 0.5;//0.3と3
+let lambda = 3;//0.3と3
 let testAmp = 1;//いったんここ１で固定する
 let calcCount = Math.max(1,0.01/dt);
 const defaultDamping = 0; // 減衰率
@@ -34,7 +34,7 @@ let materialAbsorption = {
   "water": [0,  0.0, 0.5],
   "glass": [0.5, 0.0, 0],
 };
-let absw = 1;
+let absw = 8;
 function makeSettingABSW(key,template) {
   console.log('-- -- --')
   for(let i = 1;i <= absw;i++) {
@@ -45,8 +45,8 @@ function makeSettingABSW(key,template) {
     console.log(`test${key}-${i}`,template.map(a=>a*c));
   }
 }
-makeSettingABSW(1,[0, 0, 1]);
-makeSettingABSW(2,[1, 0, 0]);
+makeSettingABSW(1,[0, 0, 0.1]);
+makeSettingABSW(2,[0.1, 0, 0]);
 
 
 function test2(x,y,dx,dy,material,tr=1) {
@@ -165,7 +165,7 @@ function draw() {
       let ct = map(1-transmission[i][j], -1, 1, 0, 255);
       let cw = map(waveSpeed[i][j], 0, 1, 0, 255);
       const ab = materialAbsorption[materialType[i][j]]
-      fill(c, c, ct);
+      fill(c, c, -1);
       if(materialType[i][j].startsWith("test")) {
       fill(...ab.map(a=>a*255     *5    +c))
       //} else {
