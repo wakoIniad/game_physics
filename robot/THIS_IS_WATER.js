322b81c01878ae280d008580c282dfa4f5f65e5f
wakoIniad <s1f102400606@iniad.org>
2025-08-18 06:35:36 +0900

    リアルより物理

--- robot/THIS_IS_WATER.js
+++ robot/THIS_IS_WATER.js
@@ -1,11 +1,11 @@
 //RAINY_NOISE
 const SIZE = 64;
 const m = new Array(SIZE).fill(0).map(_=>
-new Array(SIZE).fill().map((v,i)=>Math.random()<0.001//(Math.sin(Math.random()*PI)
-));
+new Array(SIZE).fill().map(_=>
+  Math.round(Math.random())));
 function setup() {
   createCanvas(400, 400);
-  frameRate(32);
+  frameRate(8);
 }
 
 //上下左右の重みを強くして曲線的な変化にする
@@ -16,31 +16,52 @@ const WEIGHT_SUM = weight.reduce((sum,v)=>sum+v,0)
 const rule = [
 
   [
+    //1,
+    //2,//波の発生減たち
+    //3,2
+    //2,4
     0,4
+    //4,
   ],
   [
     8
+    //1,2,3,4,5,6,7
+    //0,1,2,3,4,5,6,7,8
+//4,5,6 // ここの値を大きくするとなんか変化が大雑把な感じ
   ]
 ];
-const dx = 0.01;
-const dt = 0.04//0.0005//0.0008;
-let last = m.map(arr=>[...arr]);;
-const c = 0.9;
+
 function update() {
-  //セルオートマトン部分は、画素がいい感じに動けば何でもいい
-  //ランダムな値加算 & ぼかし が重要
-  const now = m.map(arr=>[...arr]);
+  const ref = m.map(arr=>[...arr]);
   for(let i = 0;i < SIZE;i++) {
     for(let j = 0;j < SIZE;j++) {
-      
-      lap = (now[(i+1)%SIZE][j] - 2.0*now[i][j] + now[(i-1+SIZE)%SIZE][j]) / (dx)
-                 + (now[i][(j+1)%SIZE] - 2.0*now[i][j] + now[i][(j-1+SIZE)%SIZE]) / (dx);
-      m[i][j] = 2.0*now[i][j] - last[i][j] + (c*dt)*(c*dt) * lap;
+      const ts = [1, 0];
+      const p = [-1,-1];
+      let total = 0;
+      for(let _ = 0;_ < 8;_++) {
+        if(_ && _ % 2 === 0) {
+          ts.reverse();
+          if(ts[0]) {
+            ts[0] *= -1;
+          }
+        }
+        const x = p[0] + i;
+        const y = p[1] + j;
+        total += weight[_]*ref[(x + SIZE)%SIZE][(y + SIZE)%SIZE];
+        //console.log(i,j,(x + SIZE)%SIZE, (y + SIZE)%SIZE);
+        p[0] += ts[0];
+        p[1] += ts[1];
+      }
+      total = ~~(8*total/WEIGHT_SUM)
+      if(rule[0].includes(total)) {
+        m[i][j] = 1;
+      } else if(!(rule[1].includes(total)) && ref[i][j]) {
+        m[i][j] = 0;
+      }
     }  
   }
-  last = now;
    
-  /*const ref2 = m.map(arr=>[...arr]);
+  const ref2 = m.map(arr=>[...arr]);
   for(let i = 0;i < SIZE;i++) {
     for(let j = 0;j < SIZE;j++) {
       //更新タイミングの関係で
@@ -48,19 +69,18 @@ function update() {
       //if((~~ref2[i][j])!=(~~ref[i][j])){
 //      if((~~m[i][j])!=(~~ref[i][j])){
       if((~~m[i][j])!=(~~ref2[i][j])){
-        m[i][j]= myf(ref2,i,j,true);
+        m[i][j]= 1-myf(ref2,i,j,true);
       }else {
-        m[i][j] = 1-myf(ref2,i,j,true);
+        m[i][j] = myf(ref2,i,j,true);
       }
     }
-  }*/
+  }
   
   for(let i = 0;i < SIZE;i++) {
     for(let j = 0;j < SIZE;j++) {
-      //雨の強さ変えたいときはここを変更 
+      //** 雨の強さ変えたいときはここを変更 **//
         //m[i][j]+=(1-Math.random()**2-0.5)/4//10;
-        const rnd = Math.random();
-        m[i][j]+=rnd < 0.0001 ? 1: rnd > 0.9999 ? -1:0//10;
+        m[i][j]+=(1-Math.random()**2-0.5)/4//10;
     }
   }
 }
@@ -86,24 +106,18 @@ function myf(ref,i,j,removeSelf=false) {
 const MODE = //"RAW";
 "FILTERED";
 function draw() {
-  background(220/2,220/2,255);
-  //background(22,22,100);
+  background(220);
   update();
   fill(0);
   noStroke();
   for(let i = 0;i < SIZE;i++) {
     for(let j = 0;j < SIZE;j++) {
+      if(MODE == "FILTERED") {
 //         console.log(myf(i,j));
-         fill(10,10,100,myf(m,i,j)*255);
-         //fill(255,255,255,128-myf(m,i,j)*255);
-        // rect(width/SIZE * i, height/SIZE * j, width/SIZE, width/SIZE);
-      
-      //if(m[i][j]<0.5) {
-        rect(width/SIZE * i, height/SIZE * j, width/SIZE, width/SIZE);
-      //}
-      //if(m[i][j]<0.5) {
-      //  rect(width/SIZE * i, height/SIZE * j, width/SIZE, width/SIZE);
-      //}
+         fill(0,0,0,128-myf(m,i,j)*255);
+         rect(width/SIZE * i, height/SIZE * j, width/SIZE, width/SIZE);
+      } else 
+      if(m[i][j]>0.5) rect(width/SIZE * i, height/SIZE * j, width/SIZE, width/SIZE);
     }
   }
 }
---
+++ robot/t.js
@@ -0,0 +1,120 @@
+//RAINY_NOISE
+const SIZE = 64;
+const m = new Array(SIZE).fill(0).map(_=>
+new Array(SIZE).fill().map(_=>
+  Math.round(Math.random())));
+function setup() {
+  createCanvas(400, 400);
+  frameRate(8);
+}
+
+//上下左右の重みを強くして曲線的な変化にする
+const weight = [
+  1,1,2,2,4,4,8,8//,16,//1,4,2,3,3,2,4,1//-1,1,-1,2,-1,4,-1,2,-1
+]; // -> ４近傍が大きい時、最大値に近いそうでない時最小値に近い
+const WEIGHT_SUM = weight.reduce((sum,v)=>sum+v,0)
+const rule = [
+
+  [
+    //1,
+    //2,//波の発生減たち
+    //3,2
+    //2,4
+    0,4
+    //4,
+  ],
+  [
+    8
+    //1,2,3,4,5,6,7
+    //0,1,2,3,4,5,6,7,8
+//4,5,6 // ここの値を大きくするとなんか変化が大雑把な感じ
+  ]
+];
+
+function update() {
+  const ref = m.map(arr=>[...arr]);
+  for(let i = 0;i < SIZE;i++) {
+    for(let j = 0;j < SIZE;j++) {
+      const ts = [1, 0];
+      const p = [-1,-1];
+      let total = 0;
+      for(let _ = 0;_ < 8;_++) {
+        if(_ && _ % 2 === 0) {
+          ts.reverse();
+          if(ts[0]) {
+            ts[0] *= -1;
+          }
+        }
+        const x = p[0] + i;
+        const y = p[1] + j;
+        total += weight[_]*ref[(x + SIZE)%SIZE][(y + SIZE)%SIZE];
+        //console.log(i,j,(x + SIZE)%SIZE, (y + SIZE)%SIZE);
+        p[0] += ts[0];
+        p[1] += ts[1];
+      }
+      total = ~~(8*total/WEIGHT_SUM)
+      if(rule[0].includes(total)) {
+        m[i][j] = 1;
+      } else if(!(rule[1].includes(total)) && ref[i][j]) {
+        m[i][j] = 0;
+      }
+    }  
+  }
+   
+  const ref2 = m.map(arr=>[...arr]);
+  for(let i = 0;i < SIZE;i++) {
+    for(let j = 0;j < SIZE;j++) {
+      //if((~~ref2[i][j])!=(~~ref[i][j])){
+//      if((~~m[i][j])!=(~~ref[i][j])){
+      if((~~m[i][j])!=(~~ref[i][j])){
+        m[i][j]= myf(ref,i,j,true);
+      }else {
+        m[i][j] = 1-myf(ref,i,j,true);
+      }
+    }
+  }
+  
+  for(let i = 0;i < SIZE;i++) {
+    for(let j = 0;j < SIZE;j++) {
+      //** 雨感減らしたいときはここを変更 **//
+        m[i][j]+=(1-Math.random()**2-0.5)/4//10;
+    }
+  }
+}
+
+function myf(ref,i,j,removeSelf=false) {
+    let total = 0;
+    const filterWeight = [
+        1,2,1,
+        2,4,2,
+        1,2,1
+    ].map(v=>v/(removeSelf?12:16));
+    let at = 0;
+    for(let s = -1;s < 2; s++) {
+        for(let t = -1;t < 2; t++) {
+            if(!(s||t) && removeSelf)continue;
+            total += filterWeight[at] * 
+            ref[((j+t) + SIZE)%SIZE][((i+s) + SIZE)%SIZE];
+            at++;
+        }
+    }
+    return total;
+}
+const MODE = //"RAW";
+"FILTERED";
+function draw() {
+  background(220);
+  update();
+  fill(0);
+  noStroke();
+  for(let i = 0;i < SIZE;i++) {
+    for(let j = 0;j < SIZE;j++) {
+      if(MODE == "FILTERED") {
+//         console.log(myf(i,j));
+         fill(0,0,0,myf(m,i,j)*255);
+         rect(width/SIZE * i, height/SIZE * j, width/SIZE, width/SIZE);
+      } else 
+      if(m[i][j]>0.5) rect(width/SIZE * i, height/SIZE * j, width/SIZE, width/SIZE);
+    }
+  }
+}
