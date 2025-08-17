6f54c2ab5723f25bfdea1cd0a1be45f5ab16d24f
wakoIniad <s1f102400606@iniad.org>
2025-08-18 07:39:41 +0900

    save

--- robot/version2.js
+++ robot/version2.js
@@ -1,16 +1,16 @@
 //RAINY_NOISE
-const SIZE = 128;
+const SIZE = 64;
 const m = new Array(SIZE).fill(0).map(_=>
 new Array(SIZE).fill().map(_=>
   Math.round(Math.random())));
 function setup() {
   createCanvas(400, 400);
-  frameRate(8);
+  frameRate(5);
 }
 
 //上下左右の重みを強くして曲線的な変化にする
 const weight = [
-  2,1,2,1,2,1,2,1,2//1,1,2,2,4,4,8,8//,16,//1,4,2,3,3,2,4,1//-1,1,-1,2,-1,4,-1,2,-1
+  1,1,2,2,4,4,8,8//,16,//1,4,2,3,3,2,4,1//-1,1,-1,2,-1,4,-1,2,-1
 ]; // -> ４近傍が大きい時、最大値に近いそうでない時最小値に近い
 const WEIGHT_SUM = weight.reduce((sum,v)=>sum+v,0)
 const rule = [
@@ -20,14 +20,11 @@ const rule = [
     //2,//波の発生減たち
     //3,2
     //2,4
-    //0,4
-    //1,2,3
+    0,4
     //4,
-    0
   ],
   [
-    1
-    //8
+    8
     //1,2,3,4,5,6,7
     //0,1,2,3,4,5,6,7,8
 //4,5,6 // ここの値を大きくするとなんか変化が大雑把な感じ
@@ -100,11 +97,10 @@ function draw() {
  const col = new Array(SIZE).fill(0).map(_=>
  new Array(SIZE).fill().map(_=>220));
   update();
-  update();
   noStroke();
   for(let i = 0;i < SIZE;i++) {
     for(let j = 0;j < SIZE;j++) {
-        col[i][j] = 0.5+m[i][j]/2;
+        col[i][j] = 0.5+myf(m,i,j)/2;
         fill(255*col[i][j]);
         rect(width/SIZE * i, height/SIZE * j, width/SIZE, width/SIZE);
     }
