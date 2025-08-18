dfa1b26aba01b6ca58731811ecb6aa11da669a99
wakoIniad <s1f102400606@iniad.org>
2025-08-18 09:27:42 +0900

    save

--- robot/finish/少し荒い海.js
+++ robot/finish/少し荒い海.js
@@ -14,11 +14,13 @@ const weight = [
 ]; 
 const WEIGHT_SUM = weight.reduce((sum,v)=>sum+v,0)
 const rule = [
+
   [
     0,2
   ],
   [
     1,4
+
   ]
 ];
 
@@ -37,17 +39,34 @@ function update() {
   for(let i = 0;i < SIZE;i++) {
     for(let j = 0;j < SIZE;j++) {
       let total = 0;
+      const ts = [1, 0];
+      const p = [-1,-1];
+      for(let _ = 0;_ < 8;_++) {
+        if(_ && _ % 2 === 0) {
+          ts.reverse();
+          if(ts[0]) {
+            ts[0] *= -1;
+          }
+        }
+        const x = p[0] + i;
+        const y = p[1] + j;
+        total += weight[_]*ref[(x + SIZE)%SIZE][(y + SIZE)%SIZE];
+        p[0] += ts[0];
+        p[1] += ts[1];
+      }
+      total = 0;
       for(let at = 0;at < 8;at++) {
         const p = neighbors[at];
-        const x = (p[0] + i + SIZE)%SIZE;
-        const y = (p[1] + j + SIZE)%SIZE;
-        total += weight[at]*ref[x][y];
+        const x = p[0] + i;
+        const y = p[1] + j;
+        total += weight[at]*ref[(x + SIZE)%SIZE][(y + SIZE)%SIZE];
         
       }
-      total = Math.floor(8*total/WEIGHT_SUM);
+      //koko ijiru omoshiro
+      total = ~~(8*total/WEIGHT_SUM)
       if(rule[0].includes(total) && m[i][j] < 0.5) {
-        m[i][j] += 2;
-      } else if(!(rule[1].includes(total)) && m[i][j] > 0.5) {
+        m[i][j] += 1;
+      } else if(!(rule[1].includes(total)) && m[i][j] > 0.5/*ref[i][j]*/) {
         m[i][j] -= 1;
       }
     }  
@@ -88,13 +107,12 @@ function draw() {
   
  const col = new Array(SIZE).fill(0).map(_=>
  new Array(SIZE).fill().map(_=>220));
-  //2回やるの重要
   update();
   update();
   noStroke();
   for(let i = 0;i < SIZE;i++) {
     for(let j = 0;j < SIZE;j++) {
-        col[i][j] = 0.5+myf(m,i,j)/2;
+        col[i][j] = 0.5+/*m[i][j]*/myf(m,i,j)/2;
         fill(255*col[i][j]);
         rect(width/SIZE * i, height/SIZE * j, width/SIZE, width/SIZE);
     }
--- robot/korenikettei.js
+++ robot/korenikettei.js
@@ -10,44 +10,55 @@ function setup() {
 
 //上下左右の重みを強くして曲線的な変化にする
 const weight = [
-  2,1,2,1,2,1,2,1,2
-]; 
+  2,1,2,1,2,1,2,1,2//1,1,2,2,4,4,8,8//,16,//1,4,2,3,3,2,4,1//-1,1,-1,2,-1,4,-1,2,-1
+]; // -> ４近傍が大きい時、最大値に近いそうでない時最小値に近い
 const WEIGHT_SUM = weight.reduce((sum,v)=>sum+v,0)
 const rule = [
+
   [
+    //1,
+    //2,//波の発生減たち
+    //3,2
+    //2,4
+    //0,4
+    //1,2,3
+    //4,
     0,2
   ],
   [
     1,4
+    //8
+    //1,2,3,4,5,6,7
+    //0,1,2,3,4,5,6,7,8
+//4,5,6 // ここの値を大きくするとなんか変化が大雑把な感じ
   ]
 ];
 
-const neighbors = [
-  [-1,-1],
-  [-1, 0],
-  [-1, 1],
-  [ 0, 1],
-  [ 1, 1],
-  [ 1, 0],
-  [ 1,-1],
-  [ 0,-1],
-]
 function update() {
   const ref = m.map(arr=>[...arr]);
   for(let i = 0;i < SIZE;i++) {
     for(let j = 0;j < SIZE;j++) {
+      const ts = [1, 0];
+      const p = [-1,-1];
       let total = 0;
-      for(let at = 0;at < 8;at++) {
-        const p = neighbors[at];
-        const x = (p[0] + i + SIZE)%SIZE;
-        const y = (p[1] + j + SIZE)%SIZE;
-        total += weight[at]*ref[x][y];
-        
+      for(let _ = 0;_ < 8;_++) {
+        if(_ && _ % 2 === 0) {
+          ts.reverse();
+          if(ts[0]) {
+            ts[0] *= -1;
+          }
+        }
+        const x = p[0] + i;
+        const y = p[1] + j;
+        total += weight[_]*ref[(x + SIZE)%SIZE][(y + SIZE)%SIZE];
+        //console.log(i,j,(x + SIZE)%SIZE, (y + SIZE)%SIZE);
+        p[0] += ts[0];
+        p[1] += ts[1];
       }
-      total = Math.floor(8*total/WEIGHT_SUM);
+      total = ~~(8*total/WEIGHT_SUM)
       if(rule[0].includes(total) && m[i][j] < 0.5) {
-        m[i][j] += 2;
-      } else if(!(rule[1].includes(total)) && m[i][j] > 0.5) {
+        m[i][j] += 1;
+      } else if(!(rule[1].includes(total)) && m[i][j] > 0.5/*ref[i][j]*/) {
         m[i][j] -= 1;
       }
     }  
@@ -88,13 +99,12 @@ function draw() {
   
  const col = new Array(SIZE).fill(0).map(_=>
  new Array(SIZE).fill().map(_=>220));
-  //2回やるの重要
   update();
   update();
   noStroke();
   for(let i = 0;i < SIZE;i++) {
     for(let j = 0;j < SIZE;j++) {
-        col[i][j] = 0.5+myf(m,i,j)/2;
+        col[i][j] = 0.5+/*m[i][j]*/myf(m,i,j)/2;
         fill(255*col[i][j]);
         rect(width/SIZE * i, height/SIZE * j, width/SIZE, width/SIZE);
     }
